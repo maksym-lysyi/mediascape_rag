ee77e18cf6799d56d5924f5f1652d06d664be03f
Maksym <maksym.lysyi@litslink.com>
2024-08-07 13:23:09 +0300

    add streamlit interface

--- main.py
+++ main.py
@@ -1,33 +1,52 @@
+import os
+import asyncio
+import logging
 from fastapi import FastAPI
 from apify_client import ApifyClientAsync
+from dotenv import load_dotenv
 from preprocessing import get_urls, colect_crawled_content, init_retriever, init_llm, get_answer, get_text
-import asyncio
-import os
+from pydantic import BaseModel
 
 
-groq_key = os.getenv("GROQ_KEY")
-apify_api = os.getenv("APIFY_API")
+load_dotenv()
+groq_key = os.getenv("GROQ_API_KEY")
+apify_api = os.getenv("APIFY_API_KEY")
+
+logging.basicConfig(
+    level=logging.INFO, 
+    filename="log.log", 
+    filemode="w",
+    format='%(asctime)s, %(levelname)s, %(message)s'
+)
+
+logger = logging.getLogger(__name__)
 
 client = ApifyClientAsync(apify_api)
 app = FastAPI()
 
 semaphore = asyncio.Semaphore(4) 
 
-@app.post("/test/")
-async def main(question: str):
-    urls = get_urls(question=question)
-    print("We have URLs")
+class UserInput(BaseModel):
+    question: str
 
-    # results = get_text(urls)  ||  faster method for testing
-    results = await colect_crawled_content(urls, client, semaphore)
-    print("We have context")
+class ModelOutput(BaseModel):
+    response: str
+
+@app.post("/test/")
+async def main(input: UserInput):
+    urls = get_urls(question=input.question)
+    logger.info(f"We have URLs: {urls}")
+
+    # parsed_text = get_text(urls)  # faster method for testing
+    parsed_text = await colect_crawled_content(urls, client, semaphore)
+    logger.info(f"We have context with length {len(parsed_text)}")
 
     llm = init_llm(api_key=groq_key)
-    print("We have llm")
+    logger.info("We have LLM")
 
-    frm = init_retriever(text=results)
-    print("We have rm")
+    frm = init_retriever(text=parsed_text)
+    logger.info("We have retriever model")
 
-    answer = get_answer(question=question, frm=frm, llm=llm)
+    response = get_answer(question=input.question, frm=frm, llm=llm)
 
-    return answer
+    return ModelOutput(response=response)
---
+++ streamlit_app.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import requests
+
+st.title("LLM Chat based on the information from Mediascape site.")
+
+# Display the initial prompt from the model
+initial_prompt = "How can I help you?"
+
+# Display the initial prompt
+st.chat_message("bot").write(initial_prompt)
+
+# Input field for the user to enter their question
+question = st.text_input("Enter your question:")
+
+# Button to submit the question
+if st.button("Submit"):
+    if question:
+        # Display the user's question
+        st.chat_message("user").write(question)
+        
+        # Make a POST request to the FastAPI endpoint
+        response = requests.post("http://fastapi:8000/test/", json={"question": question})
+        
+        # Check the response status
+        if response.status_code == 200:
+            answer = response.json()["response"]
+            # Display the response from the model
+            st.chat_message("bot").write(answer)
+        else:
+            error_message = "Error: " + response.text
+            # Display the error message
+            st.chat_message("bot").write(error_message)
